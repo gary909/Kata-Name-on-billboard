155a58d6a26acfc32f0fc209791682c4293f194b
gary <gary.white@lodgit.com>
2022-05-31 12:53:57 +0200

    code

---
+++ app.js
@@ -0,0 +1,12 @@
+function billboard(name, price = 30) {
+
+}
+
+console.log(billboard("Jeong-Ho Aristotelis")); // 600
+console.log(billboard("Abishai Charalampos")); // 570
+console.log(billboard("Idwal Augustin")); // 420
+console.log(billboard("Hadufuns John", 20)); // 260
+console.log(billboard("Zoroaster Donnchadh")); // 570
+console.log(billboard("Claude Miljenko")); // 450
+console.log(billboard("Werner Vígi", 15)); // 165
+console.log(billboard("Anani Fridumar")); // 420
